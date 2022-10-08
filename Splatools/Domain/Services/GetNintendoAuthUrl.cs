a5f1140e46ad52d9c947bbb7554604cea4a89c08
ClosetheWorld <develop@closetheworld.net>
2022-10-09 03:39:09 +0900

    :bug: fix challenge to verifier

--- Splatools/Domain/Services/GetNintendoAuthUrl.cs
+++ Splatools/Domain/Services/GetNintendoAuthUrl.cs
@@ -29,7 +29,7 @@ public class GetNintendoAuthUrl : IGetNintendoAuthUrl
         await CallInsertAuthenticationParameterAsync(new AuthenticationParameter()
         {
             Key = key,
-            Challenge = challenge,
+            Verifier = verifier,
             InsertionTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
         });
 
---
+++ Splatools/Migrations/20221008183734_fix-AuthenticationParameters-Challenge-to-Verifier.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Splatools.Migrations
+{
+    public partial class fixAuthenticationParametersChallengetoVerifier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Challenge",
+                table: "AuthenticationParameters",
+                newName: "Verifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Verifier",
+                table: "AuthenticationParameters",
+                newName: "Challenge");
+        }
+    }
+}
